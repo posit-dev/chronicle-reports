c9f67988b2e6084229d172a1403411015193681b
matt-urbina <matt.urbina@posit.co>
2025-12-29 11:45:38 -0600

    update user guids to match number of users

--- R/sample-data.R
+++ R/sample-data.R
@@ -98,10 +98,7 @@ sample_connect_user_list_internal <- function() {
     date = as.Date(rep("2024-01-03", 26)),
     username = paste0("user", sprintf("%02d", 1:26)),
     id = paste0(
-      "user-guid-001",
-      "user-guid-002",
-      "user-guid-003",
-      "user-guid-004"
+      sprintf("user-guid-%03d", 1:26)
     ),
     email = paste0("user", 1:26, "@example.com"),
     first_name = paste0("First", 1:26),
