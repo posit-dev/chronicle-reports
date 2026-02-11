1746b218f916981f7450f3360844069f818ae144
matt-urbina <matt.urbina@posit.co>
2026-02-11 12:18:59 -0600

    try reexport

--- R/chronicle-reports-package.R
+++ R/chronicle-reports-package.R
@@ -6,8 +6,34 @@
 ## usethis namespace: end
 NULL
 
-.onLoad <- function(libname, pkgname) {
-  if (!("package:dplyr" %in% search())) {
-    do.call("library", list("dplyr"))
-  }
-}
+# Re-export commonly used dplyr verbs so users don't need library(dplyr)
+#' @importFrom dplyr collect filter select mutate group_by summarise arrange
+#' @importFrom dplyr slice ungroup pull desc n left_join inner_join
+#' @export
+dplyr::collect
+#' @export
+dplyr::filter
+#' @export
+dplyr::select
+#' @export
+dplyr::mutate
+#' @export
+dplyr::group_by
+#' @export
+dplyr::summarise
+#' @export
+dplyr::arrange
+#' @export
+dplyr::slice
+#' @export
+dplyr::ungroup
+#' @export
+dplyr::pull
+#' @export
+dplyr::desc
+#' @export
+dplyr::n
+#' @export
+dplyr::left_join
+#' @export
+dplyr::inner_join
