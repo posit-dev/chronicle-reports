1ef1e097befc5bc0bce44bedb43ce7029b6adf01
joshyam-k <josh.yamamoto@posit.co>
2026-06-03 13:07:29 -0700

    sanity check

--- R/chronicle-reports-package.R
+++ R/chronicle-reports-package.R
@@ -5,3 +5,7 @@
 ## usethis namespace: start
 ## usethis namespace: end
 NULL
+
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("chronicle.reports loaded (aws-oauth-test branch)")
+}
